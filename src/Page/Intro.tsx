1c13a35102ed67f9f7640fe0875778efb40ac206
Christian Cordeiro <christiancordeiro96@gmail.com>
2025-03-29 03:22:59 -0300

    consuming youtube api

--- src/Page/Intro.tsx
+++ src/Page/Intro.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from 'react'
 import name from '../assets/Name.svg'
 import g from '../assets/gifs/aaaaa.gif'
 import vignette from '../assets/vignette.png'
 
 const Intro = () => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet,liveStreamingDetails&id=jfKfPfyJRdk&key=${apiKey}`
+
+  useEffect(() => {
+    async function req() {
+      try {
+        const response = await fetch(apiUrl)
+        if (!response.ok) {
+          throw new Error(`Erro na solicitação: ${response.status}`)
+        }
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.error('Erro ao buscar os dados:', error)
+      }
+    }
+    req()
+  })
+
   return (
     <main>
       <div className="flex items-center justify-center h-screen">
